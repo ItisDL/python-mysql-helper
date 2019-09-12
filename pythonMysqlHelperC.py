afc5996e42817143c145725c9454e14ffbc43236
dl <948142764@qq.com>
2019-09-12 16:42:50 +0800

    no message

--- pythonMysqlHelperC.py
+++ pythonMysqlHelperC.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # by dl
 import pymysql
+import sys
 import traceback
 from config import conf
 
@@ -11,44 +12,108 @@ class pythonMysqlHepler:
                  password=conf.my_password,
                  db=conf.my_db,
                  port=conf.my_port,
-                 charset=conf.my_charset,
-                 dict=1):
+                 charset=conf.my_charset):
         self.host = host
         self.user = user
         self.password = password
         self.db = db
-        self.port=port
-        self.charset=charset
+        self.port = port
+        self.charset = charset
         self.con = pymysql.connect(host=self.host,
                                    user=self.user,
                                    password=self.password,
                                    db=self.db,
-                                   port=self.db,
-                                   charset = self.charset
+                                   port=self.port,
+                                   charset=self.charset
                                    )
-    # @staticmethod
-    def update(self,table,data,where):
-        cur = self.con.cursor()
-        # set项
-        setStr = ''
-        for _ in data:
-            setStr += _ + '=%s,'
-        setStr = setStr[:-1]
-        # print(setStr)
-        # where项
-        whereStr = where
-        # whereStr = ' where '
-        # for _ in where:
-        # whereStr += _ + '=' + where
-        # 拼接
-        sql = 'update ' + table + ' set ' + setStr + whereStr
-        print(sql)
+
+
+    def createCur(self, funcName=''):
+        if funcName in ['getOne', 'getCol']:
+            self.cur = self.con.cursor()
+        else:
+            self.cur = self.con.cursor(cursor=pymysql.cursors.DictCursor)
+
+    def executeSql(self, sql, params):
+        if params:
+            result = self.cur.execute(sql, params)
+        else:
+            result = self.cur.execute(sql)
+        return result
+
+    def getOne(self, sql, params=()):
+        self.createCur(sys._getframe().f_code.co_name)
+        self.executeSql(sql + ' limit 1', params)
+        results = self.cur.fetchone()
+        if results:
+            return results[0]
+        else:
+            return results
+
+    def getRow(self, sql, params=()):
+        self.createCur(sys._getframe().f_code.co_name)
+        self.executeSql(sql + ' limit 1', params)
+        results = self.cur.fetchone()
+        return results
+
+    def getCol(self, sql, params=()):
+        self.createCur(sys._getframe().f_code.co_name)
+        self.cur.execute(sql, params)
+        results = self.cur.fetchall()
+        if results:
+            return [x[0] for x in results]
+        else:
+            return results
+
+    def getAll(self, sql, params=()):
+        self.executeSql(sql, params)
+        results = self.cur.fetchall()
+        return results
+
+    def exec(self, sql, params=()):
+        result = self.executeSql(sql, params)
+        return result
+
+    def insert(self, table, data):
+        colStr = '(' + ','.join(data.keys()) + ')'
+        vDataStr = ''
+        for _ in data.keys():
+            vDataStr += '%s,'
+        vDataStr = vDataStr[:-1]
+        vDataStr = '(' + vDataStr + ')'
+        sql = 'insert into ' + table + colStr + ' values ' + vDataStr
         params = list(data.values())
-        results = cur.execute(sql, params)
+        results = self.cur.execute(sql, params)
         self.con.commit()
         return results
 
+    def update(self, table, data, where):
+        setStr = ''
+        for _ in data:
+            setStr += _ + ' = %s,'
+        setStr = setStr[:-1]
+        whereStr = ' where ' + where
+        sql = 'update ' + table + ' set ' + setStr + whereStr
+        params = list(data.values())
+        results = self.cur.execute(sql, params)
+        self.con.commit()
+        return results
+
+
 if __name__ == '__main__':
-    # 没有实例化直接用会报错 实例化 可以直接在类名后面加一个()
-    res = pythonMysqlHepler().update(table='user_shop',data={'name':'DLTest'},where='id = 1')
-    print(res)
+    # res = pythonMysqlHepler().insert(table='user_shop', data={'name': 'DLInsert'})
+    # print(res)
+    # 没有实例化直接用会报错 不想实例化可以直接在类名后面加一个()
+    # res = pythonMysqlHepler().update(table='user_shop', data={'name': 'DLTest'}, where='id = 2')
+    # print(res)
+    # res = pythonMysqlHepler().exec(sql='delete from user_shop where id = 1')
+    # print(res)
+    # res = pythonMysqlHepler().getAll(sql='select * from user_shop')
+    # print(res)
+    # res = pythonMysqlHepler().getOne(sql='select id from user_shop where id =2')
+    # print(res)
+    # res = pythonMysqlHepler().getRow(sql='select * from user_shop')
+    # print(res)
+    # res = pythonMysqlHepler().getCol(sql='select name from user_shop')
+    # print(res)
+    pass
